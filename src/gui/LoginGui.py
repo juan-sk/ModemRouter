ede6b83705e3bea954b2dccd5a197cc461ca875b
juan-sk <juan.ramirez.sk@gmail.com>
2022-06-11 13:21:37 -0400

    cambio gestion de contrasña

--- src/gui/LoginGui.py
+++ src/gui/LoginGui.py
@@ -1,6 +1,7 @@
 from controller.LoginController import LoginController
 from entity.UsuarioEntity import UsuarioEntity
 from utils.GuiUtils import GuiUtils
+import getpass
 
 
 class LoginGui:
@@ -9,17 +10,19 @@ class LoginGui:
         self.usuarioController  = LoginController()
     
     def login(self):
-        print("Porfavor ingrese sus credenciales")
+        print("Por Favor Ingrese sus Credenciales")
 
         usuario = UsuarioEntity()
         while True:
             usuario = input("Usuario:")
-            password =input("Password:")
+            password =  getpass.getpass()
             
             usuario = self.usuarioController.login(usuario,password)
             if(usuario):
                 GuiUtils.clearTerminal()
                 print("Bienvenido %s"% usuario.nombreUsuario)
                 break
+            else:
+                print("Credenciales Incorrectas... intente nuevamente")
         return usuario
           
